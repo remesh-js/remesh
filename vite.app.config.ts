3765cc586398b63f080e8282bf1b91006f7072e9
Jade Gu <guyingjie129@163.com>
2021-11-05 18:17:42 +0800

    chore: add 7guis

--- vite.app.config.ts
+++ vite.app.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vacation-biz-ui/',
   plugins: [reactRefresh(), vanillaExtractPlugin()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        'todo-app': resolve(__dirname, 'todo-app/index.html')
+      }
+    }
+  }
 });
