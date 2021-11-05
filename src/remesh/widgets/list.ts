85890267a62fb66eb66b3a75b349860c6db0c0d2
yj_gu <yj_gu@trip.com>
2021-11-05 19:09:10 +0800

    refactor: optimize remesh api

--- src/remesh/widgets/list.ts
+++ src/remesh/widgets/list.ts
@@ -1,128 +1,123 @@
-import { merge } from 'rxjs'
-import { map } from 'rxjs/operators'
-import { Remesh } from '../remesh'
-
+import { merge } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Remesh } from '../remesh';
 
 export type ListWidgetOptions<T> = {
-  name: string
-  getKey: (item: T) => string
-  createItem: (key: string) => T
-}
+  name: string;
+  getKey: (item: T) => string;
+  createItem: (key: string) => T;
+};
 
 export const ListWidget = <T>(options: ListWidgetOptions<T>) => {
-  return Remesh.widget(domain => {
-
+  return Remesh.widget((domain) => {
     const KeyListState = domain.state<string[]>({
       name: `${options.name}.KeyListState`,
-      default: []
-    })
+      default: [],
+    });
 
     const ItemState = domain.state<string, T>({
       name: `${options.name}.ItemState`,
-      impl: key => {
-        return options.createItem(key)
-      }
-    })
+      impl: (key) => {
+        return options.createItem(key);
+      },
+    });
 
     const KeyListQuery = domain.query({
       name: `${options.name}.KeyListQuery`,
       impl: ({ get }) => {
-        return get(KeyListState())
-      }
-    })
-
+        return get(KeyListState());
+      },
+    });
 
     const ItemQuery = domain.query({
       name: `${options.name}.ItemQuery`,
       impl: ({ get }, key: string) => {
-        return get(ItemState(key))
-      }
-    })
+        return get(ItemState(key));
+      },
+    });
 
     const ItemListQuery = domain.query({
       name: `${options.name}.ItemListQuery`,
-      impl: ({ query }) => {
-        return query(KeyListQuery()).map(key => query(ItemQuery(key)))
-      }
-    })
+      impl: ({ get }) => {
+        return get(KeyListQuery()).map((key) => get(ItemQuery(key)));
+      },
+    });
 
     const setList = domain.command({
       name: `${options.name}.setList`,
-      impl: ({ set }, list: T[]) => {
-        const keyList = list.map(options.getKey)
+      impl: ({}, list: T[]) => {
+        const keyList = list.map(options.getKey);
 
         return [
-          set(KeyListState().new(keyList)),
-          ...list.map(item => set(ItemState(options.getKey(item)).new(item)))
-        ]
-      }
-    })
+          KeyListState().new(keyList),
+          ...list.map((item) => ItemState(options.getKey(item)).new(item)),
+        ];
+      },
+    });
 
     const addItem = domain.command({
       name: `${options.name}.addItem`,
-      impl: ({ query, set }, newItem: T) => {
-        const keyList = query(KeyListQuery())
-        const newKey = options.getKey(newItem)
+      impl: ({ get }, newItem: T) => {
+        const keyList = get(KeyListQuery());
+        const newKey = options.getKey(newItem);
 
         if (keyList.includes(newKey)) {
-          return []
+          return [];
         }
 
         return [
-          set(KeyListState().new(keyList.concat(newKey))),
-          set(ItemState(newKey).new(newItem)),
-        ]
-      }
-    })
+          KeyListState().new(keyList.concat(newKey)),
+          ItemState(newKey).new(newItem),
+        ];
+      },
+    });
 
     const removeItem = domain.command({
       name: `${options.name}.removeItem`,
-      impl: ({ get, set }, targetKey: string) => {
-        const keyList = get(KeyListState())
-        const newKeyList = keyList.filter(key => key !== targetKey)
+      impl: ({ get }, targetKey: string) => {
+        const keyList = get(KeyListState());
+        const newKeyList = keyList.filter((key) => key !== targetKey);
 
-        return set(KeyListState().new(newKeyList))
-      }
-    })
-
+        return KeyListState().new(newKeyList);
+      },
+    });
 
     const updateItem = domain.command({
       name: `${options.name}.updateItem`,
-      impl: ({ get, set }, newItem: T) => {
-        const key = options.getKey(newItem)
-        const keyList = get(KeyListState())
+      impl: ({ get }, newItem: T) => {
+        const key = options.getKey(newItem);
+        const keyList = get(KeyListState());
 
         if (!keyList.includes(key)) {
-          return addItem(newItem)
+          return addItem(newItem);
         }
 
-        return set(ItemState(key).new(newItem))
-      }
-    })
-
+        return ItemState(key).new(newItem);
+      },
+    });
 
     const autorun = domain.task({
       name: `${options.name}.autorun`,
       impl: ({ fromEvent }) => {
         const setList$ = fromEvent(setList.Event).pipe(
-          map(todoContent => setList(todoContent))
-        )
+          map((todoContent) => setList(todoContent))
+        );
 
         const addItem$ = fromEvent(addItem.Event).pipe(
-          map(todoContent => addItem(todoContent))
-        )
+          map((todoContent) => addItem(todoContent))
+        );
 
         const removeItem$ = fromEvent(removeItem.Event).pipe(
-          map(todoContent => removeItem(todoContent))
-        )
+          map((todoContent) => removeItem(todoContent))
+        );
 
         const updateItem$ = fromEvent(updateItem.Event).pipe(
-          map(todoContent => updateItem(todoContent))
-        )
+          map((todoContent) => updateItem(todoContent))
+        );
 
-        return merge(setList$, addItem$, removeItem$, updateItem$)
-      }
-    })
+        return merge(setList$, addItem$, removeItem$, updateItem$);
+      },
+    });
 
     return {
       autorun: [autorun],
@@ -130,13 +125,13 @@ export const ListWidget = <T>(options: ListWidgetOptions<T>) => {
         setLIst: setList.Event,
         addItem: addItem.Event,
         removeItem: removeItem.Event,
-        updateItem: updateItem.Event
+        updateItem: updateItem.Event,
       },
       query: {
         KeyListQuery,
         ItemListQuery,
         ItemQuery,
-      }
-    }
-  })()
-}
+      },
+    };
+  })();
+};
