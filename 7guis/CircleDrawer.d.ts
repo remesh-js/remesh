e7abb802c5ea2c8c083b459326d0fb331ed6f03d
Jade Gu <guyingjie129@163.com>
2022-03-17 22:26:03 +0800

    chore: restructure

--- 7guis/CircleDrawer.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="react" />
-export declare const CircleDrawerApp: () => JSX.Element;
--- 7guis/TemperatureConverter.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="react" />
-export declare const TemperatureConverterApp: () => JSX.Element;
