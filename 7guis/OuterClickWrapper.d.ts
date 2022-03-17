2b242f9ae17c6103e09d196d65da7db83ab21440
yj_gu <yj_gu@trip.com>
2022-03-17 22:26:03 +0800

    chore: restructure

--- 7guis/OuterClickWrapper.d.ts
+++
@@ -1,6 +0,0 @@
-import { ComponentPropsWithoutRef } from 'react';
-export declare const OuterClickWrapper: (props: OuterClickWrapperProps) => JSX.Element;
-declare type OuterClickWrapperProps = ComponentPropsWithoutRef<'div'> & {
-    onOuterClick?: (event: MouseEvent) => void;
-};
-export {};
