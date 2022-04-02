acf6a8c2a426a540199d72f2134326b8d81581fb
Jade Gu <guyingjie129@163.com>
2022-04-02 20:09:52 +0800

    chore: clean build output

--- packages/remesh-logger/dist/remesh-logger.d.ts
+++
@@ -1,6 +0,0 @@
-import { RemeshStoreInspector } from 'remesh';
-import { RemeshDebugOptions } from 'remesh-debugger-helper';
-export declare type RemeshLoggerOptions = RemeshDebugOptions & {
-    collapsed?: boolean;
-};
-export declare const RemeshLogger: (options?: RemeshLoggerOptions | undefined) => RemeshStoreInspector;
