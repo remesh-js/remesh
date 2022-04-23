a445c33594d75041035c384e40ccc26868d03a69
cijiugechu <cijiugechu@gmail.com>
2022-04-23 15:43:29 +0800

    feat: port Cells

--- projects/vue-demos/src/7guis/Cells.vue
+++ projects/vue-demos/src/7guis/Cells.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useRemeshDomain, useRemeshQuery } from 'remesh-vue'
 import { Cells } from 'remesh-domains-for-demos/dist/7guis/Cells'
+import RowView from './RowView.vue'
 
   const cells = useRemeshDomain(Cells())
   const columnKeyList = useRemeshQuery(cells.query.columnKeyList())
@@ -10,5 +11,37 @@ import { Cells } from 'remesh-domains-for-demos/dist/7guis/Cells'
 </script>
 
 <template>
-
+<div>
+      <h2>Cells</h2>
+      <table
+        :style="{
+          borderCollapse: 'collapse',
+          border: '1px solid #bbb',
+          textAlign: 'center',
+        }"
+      >
+        <thead>
+          <tr
+            :style="{
+              backgroundColor: '#f6f6f6',
+            }"
+          >
+            <th :style="{ width: '30px', display: 'block' }"></th>
+              <th v-for="key of columnKeyList" :key="key"
+                :style="{
+                  maxWidth: '80px',
+                  border: '1px solid #bbb',
+                }"
+              >
+                {{ key }}
+              </th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr v-for="rowKey of rowKeyList" :key="rowKey">
+              <RowView :rowKey="rowKey" :columnKeyList="columnKeyList" />
+            </tr>
+        </tbody>
+      </table>
+    </div>
 </template>
---
+++ projects/vue-demos/src/7guis/RowView.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { useRemeshDomain } from 'remesh-vue';
+import { Cells } from 'remesh-domains-for-demos/dist/7guis/Cells';
+import CellView from './CellView.vue';
+
+const props = defineProps<{
+    rowKey: string,
+    columnKeyList: Array<string>
+}>()
+
+const cells = useRemeshDomain(Cells())
+
+const handleClick = (event: Event, cellKey: string) => {
+    if (event.target instanceof HTMLInputElement) {
+        return
+    }
+    cells.command.selectCell(cellKey)
+}
+
+</script>
+
+<template>
+      <td
+        :style="{
+          width: '30px',
+          border: '1px solid #bbb',
+          backgroundColor: '#f6f6f6',
+        }"
+      >
+        {{ props.rowKey }}
+      </td>
+      
+          <td v-for="columnKey of props.columnKeyList" :key="`${columnKey}${props.rowKey}`"
+            :style="{
+              maxWidth: '80px',
+              minWidth: '80px',
+              border: '1px solid #bbb',
+              overflow: 'hidden',
+            }"
+            @click="handleClick($event, `${columnKey}${props.rowKey}`)"
+          >
+            <CellView :cellKey="`${columnKey}${props.rowKey}`" />
+          </td>
+</template>
