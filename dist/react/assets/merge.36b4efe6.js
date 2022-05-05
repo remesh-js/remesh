a8f55504d7bdd4e46613020ddd9b35190c423982
Jade Gu <guyingjie129@163.com>
2022-05-05 14:11:11 +0800

    chore: build example

---
+++ dist/react/assets/merge.36b4efe6.js
@@ -0,0 +1,2 @@
+import{E as o,m as s,p as t,a as u}from"./mergeAll.0f7c8167.js";import{i as g,f as p}from"./map.03340756.js";function c(){for(var r=[],e=0;e<arguments.length;e++)r[e]=arguments[e];var n=t(r),m=u(r,1/0),a=r;return a.length?a.length===1?g(a[0]):s(m)(p(a,n)):o}export{c as m};
+//# sourceMappingURL=merge.36b4efe6.js.map
