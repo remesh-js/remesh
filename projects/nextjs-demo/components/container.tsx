dc0159ff22a50216e3793bcef43be2293a7900de
Jade Gu <guyingjie129@163.com>
2022-05-06 21:38:37 +0800

    fix: fix nextjs-demos type error

--- projects/nextjs-demo/components/container.tsx
+++ projects/nextjs-demo/components/container.tsx
@@ -1,10 +1,10 @@
-import { ReactNode, FunctionComponent } from 'react'
+import React, { ReactNode, FunctionComponent } from 'react'
 
 type Props = {
   children?: ReactNode
 }
 
-const Container: FunctionComponent = ({ children }: Props) => {
+const Container: React.FC<Props> = ({ children }) => {
   return <div className="container mx-auto px-5">{children}</div>
 }
 
--- projects/nextjs-demo/components/intro.tsx
+++ projects/nextjs-demo/components/intro.tsx
@@ -1,6 +1,7 @@
+import React from 'react'
 import { CMS_NAME } from '../lib/constants'
 
-const Intro = () => {
+const Intro: React.FC = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
