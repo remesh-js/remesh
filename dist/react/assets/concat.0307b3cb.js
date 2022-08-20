90871ae1ad44f15f239d2a5fa5f66288c2e233c1
Jade Gu <guyingjie129@163.com>
2022-08-20 13:52:37 +0800

    chore: build example

---
+++ dist/react/assets/concat.0307b3cb.js
@@ -0,0 +1,2 @@
+import{m as t,p as a}from"./mergeAll.f2c6f2dd.js";import{h as n}from"./remesh-react.2e60fbbd.js";function e(){return t(1)}function s(){for(var o=[],r=0;r<arguments.length;r++)o[r]=arguments[r];return e()(n(o,a(o)))}export{s as c};
+//# sourceMappingURL=concat.0307b3cb.js.map
